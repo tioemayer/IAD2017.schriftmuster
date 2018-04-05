81c7ecd9b5f624179695d048d12d0797dcf1fc31
signalwerk <sh@signalwerk.ch>
2018-04-06 00:44:08 +0200

    ADD: remark-plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,16 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 960,
+              linkImagesToOriginal: false
+            }
+          },
           `gatsby-remark-autolink-headers`,
+          'gatsby-remark-copy-linked-files',
+
         ],
       },
     },
