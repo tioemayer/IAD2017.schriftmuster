18a256838ffbd83e08198aea557daaee90d91120
signalwerk <sh@signalwerk.ch>
2018-04-07 01:23:11 +0200

    FIX: spaces

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,19 +13,19 @@ const IndexPage = () => (
 
 
       <ul>
-        <li><Link to="/fonts/pascale">Lato </Link></li>
-        <li><Link to="/fonts/wolfgang">Rambla </Link></li>
-        <li><Link to="/fonts/natasha">Open Sans </Link></li>
-        <li><Link to="/fonts/matthias">Roboto </Link></li>
-        <li><Link to="/fonts/nils">Ubuntu </Link></li>
-        <li><Link to="/fonts/tieu">Barlow </Link></li>
-        <li><Link to="/fonts/stephanie">IBM Plex Sans </Link></li>
-        <li><Link to="/fonts/lars">Mukta Vaani </Link></li>
-        <li><Link to="/fonts/severin">Faustina </Link></li>
+        <li><Link to="/fonts/pascale">!!!! Lato</Link></li>
+        <li><Link to="/fonts/wolfgang">Rambla</Link></li>
+        <li><Link to="/fonts/natasha">Open Sans</Link></li>
+        <li><Link to="/fonts/matthias">Roboto</Link></li>
+        <li><Link to="/fonts/nils">Ubuntu</Link></li>
+        <li><Link to="/fonts/tieu">Barlow</Link></li>
+        <li><Link to="/fonts/stephanie">IBM Plex Sans</Link></li>
+        <li><Link to="/fonts/lars">Mukta Vaani</Link></li>
+        <li><Link to="/fonts/severin">Faustina</Link></li>
         <li><Link to="/fonts/marc">Cooper Hewitt</Link></li>
-        <li><Link to="/fonts/nadia">Bitter </Link></li>
-        <li><Link to="/fonts/nicole">Raleway </Link></li>
-        <li><Link to="/fonts/marius">!! Schrift von Marius</Link></li>
+        <li><Link to="/fonts/nadia">Bitter</Link></li>
+        <li><Link to="/fonts/nicole">Raleway</Link></li>
+        <li><Link to="/fonts/marius">Newton</Link></li>
       </ul>
 
 
