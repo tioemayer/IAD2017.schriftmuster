33b210780656a6118f68edfb9da43a8c9ff07063
signalwerk <sh@signalwerk.ch>
2018-04-17 15:28:26 +0200

    MOD: externalise gatsby

--- src/shared/css/breakpoints.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  m: '26rem', // 416 / 16px
-  l: '48rem', // 768 / 16px
-};
--- src/shared/css/variables.js
+++
@@ -1,7 +0,0 @@
-var breakpoints = require('./breakpoints');
-
-module.exports = {
-  // Add your css variables here.
-  'breakpoints--m': breakpoints.m,
-  'breakpoints--l': breakpoints.l,
-};
